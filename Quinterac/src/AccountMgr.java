5b82ec1d4901ab7229b98a297a5f60781e6acd3b
YingTing04 <c170130@e.ntu.edu.sg>
2019-10-17 16:44:59 -0400

    updated depositmgr, transactionfilemgr

--- Quinterac/src/AccountMgr.java
+++ Quinterac/src/AccountMgr.java
@@ -1,29 +1,27 @@
 import java.util.*;
 
 public class AccountMgr {
-	private static ArrayList <Account> dailyAccountList = new ArrayList<Account> ();
+	private static Map<String, Account> dailyAccountMap = new HashMap<String, Account>();
 	
 	public static void resetDailyAccountList() {
-		dailyAccountList.clear();
+		dailyAccountMap.clear();
 	}
 	
-	public static int checkAccountExistsInList(String accountNumber) {
-		for (int i = 0; i < dailyAccountList.size(); i++) {
-			if (dailyAccountList.get(i).getAccountNumber().equals(accountNumber)) {
-				return i;
-			}
+	public static void checkAccountExistsInList(String accountNumber) {
+		if (dailyAccountMap.containsKey(accountNumber)){
+			return;
 		}
-		addAccountToList(accountNumber);
-		return dailyAccountList.size() - 1;
+		else 
+			addAccountToList(accountNumber);
 	}
 	
 	public static void addAccountToList(String accountNumber) {
-		dailyAccountList.add(new Account(accountNumber));
+		dailyAccountMap.put(accountNumber, new Account(accountNumber));
 	}
 	
-	public static boolean checkDailyDepositLimit(String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		int dailyDeposit = dailyAccountList.get(i).getDailyDeposit();
+	public static boolean checkDailyDepositLimit(String accountNumber, int value) {
+		checkAccountExistsInList(accountNumber);
+		int dailyDeposit = dailyAccountMap.get(accountNumber).getDailyDeposit() + value;
 		
 		if (dailyDeposit <= 500000) {
 			return true;
@@ -34,14 +32,14 @@ public class AccountMgr {
 	}
 	
 	public static void performDailyDeposit (int value, String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		dailyAccountList.get(i).increaseDailyDeposit(value);
+		checkAccountExistsInList(accountNumber);
+		dailyAccountMap.get(accountNumber).increaseDailyDeposit(value);
 		System.out.println("Deposit successful!");
 	}
 	
-	public static boolean checkDailyWithdrawLimit(String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		int dailyWithdraw = dailyAccountList.get(i).getDailyWithdraw();
+	public static boolean checkDailyWithdrawLimit(String accountNumber, int value) {
+		checkAccountExistsInList(accountNumber);
+		int dailyWithdraw = dailyAccountMap.get(accountNumber).getDailyWithdraw() + value;
 		
 		if (dailyWithdraw <= 500000) {
 			return true;
@@ -52,13 +50,13 @@ public class AccountMgr {
 	}
 	
 	public static void performDailyWithdraw (int value, String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		dailyAccountList.get(i).increaseDailyWithdraw(value);
+		checkAccountExistsInList(accountNumber);
+		dailyAccountMap.get(accountNumber).increaseDailyWithdraw(value);
 	}
 	
-	public static boolean checkDailyTransferLimit(String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		int dailyTransfer = dailyAccountList.get(i).getDailyTransfer();
+	public static boolean checkDailyTransferLimit(String accountNumber, int value) {
+		checkAccountExistsInList(accountNumber);
+		int dailyTransfer = dailyAccountMap.get(accountNumber).getDailyTransfer() + value;
 		
 		if (dailyTransfer <= 1000000) {
 			return true;
@@ -69,7 +67,7 @@ public class AccountMgr {
 	}
 	
 	public static void performDailyTransfer (int value, String accountNumber) {
-		int i = checkAccountExistsInList(accountNumber);
-		dailyAccountList.get(i).increaseDailyTransfer(value);
+		checkAccountExistsInList(accountNumber);
+		dailyAccountMap.get(accountNumber).increaseDailyTransfer(value);
 	}
 }
--- Quinterac/src/DepositMgr.java
+++ Quinterac/src/DepositMgr.java
@@ -1,64 +1,79 @@
 import java.util.Scanner;
 
 public class DepositMgr {
-	private static boolean isLoggedIn = LoginMgr.isLoggedIn();
-	private static String modeName = LoginMgr.checkMode();
-	
-	public static void atmCheckDepositValid() {
-	    Scanner sc = new Scanner(System.in);
-		if (isLoggedIn == false) {
-			System.out.println("Please login first!");
-			return;
-		}
-		
-		if (modeName == "machine") {
-			System.out.println("Enter account number: ");
-			String accountNumber = sc.nextLine();
-			//check if account is valid
-			//not done
-			
-			System.out.println("Enter the amount of money to deposit in cents:");
-            int value = sc.nextInt();
-                
-            if (value >= 0 && value <= 200000) {
-            	if (AccountMgr.checkDailyDepositLimit(accountNumber)) {
-            		AccountMgr.performDailyDeposit(value, accountNumber);
-            		TransactionFileMgr.addDepTransaction(accountNumber, Integer.toString(value));
-            	}
-            	else {
-            		System.out.println("You have exceeded the daily limit");
-            	}	
-            }
-            else {
-                System.out.println("Please enter a number between 0 - 200000");	
-            }
+	private static String modeName;
+
+	public static void deposit() {
+		try {
+			if (!LoginMgr.isLoggedIn()) {
+				throw new NotLoggedInException();
+			}
+
+			modeName = LoginMgr.checkMode();
+			if (modeName.equals("machine")) {
+				atmCheckDepositValid();
+			} else if (modeName.equals("agent")) {
+				agentCheckDepositValid();
+			}
+
+		} catch (NotLoggedInException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
-	
-	public static void agentCheckDepositValid() {
-		if (isLoggedIn == false) {
-			System.out.println("Please login first!");
-			return;
-		}
-		
-		if (modeName == "agent") {
-	        Scanner sc = new Scanner(System.in);
+	public static void atmCheckDepositValid() {
+		try {
+			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter account number: ");
 			String accountNumber = sc.nextLine();
-			//check if account is valid
-			//not done
-			
+
+			if (!ValidAccListMgr.checkAccNumExist(accountNumber)) {
+				System.out.println("Please enter a valid account number");
+				return;
+			}
+
 			System.out.println("Enter the amount of money to deposit in cents:");
-            int value = sc.nextInt();
-	        
-	        if (value >= 0 && value <= 99999999) {
-	        	System.out.println("Deposit successfully:");
-	        	TransactionFileMgr.addDepTransaction(accountNumber, Integer.toString(value));
-	        }
-	        else {
-	        	System.out.println("Please enter a number between 0 - 99999999:");
-	        }
+			int value = sc.nextInt();
+
+			if (value >= 0 && value <= 200000) {
+				if (AccountMgr.checkDailyDepositLimit(accountNumber, value)) {
+					AccountMgr.performDailyDeposit(value, accountNumber);
+					TransactionFileMgr.addDepTransaction(accountNumber, Integer.toString(value));
+				} else {
+					System.out.println("You have exceeded the daily limit");
+				}
+			} else {
+				System.out.println("Please enter a number between 0 - 200000");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
+
+	}
+
+	public static void agentCheckDepositValid() {
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter account number: ");
+			String accountNumber = sc.nextLine();
+
+			if (!ValidAccListMgr.checkAccNumExist(accountNumber)) {
+				System.out.println("Please enter a valid account number");
+				return;
+			}
+
+			System.out.println("Enter the amount of money to deposit in cents:");
+			int value = sc.nextInt();
+
+			if (value >= 0 && value <= 99999999) {
+				System.out.println("Deposit successfully:");
+				TransactionFileMgr.addDepTransaction(accountNumber, Integer.toString(value));
+			} else {
+				System.out.println("Please enter a number between 0 - 99999999:");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
