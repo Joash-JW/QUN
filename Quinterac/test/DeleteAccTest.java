8a8f7f9b927585c1406b9c509f3282e5625b6d49
Joash <32333304+Joash-JW@users.noreply.github.com>
2019-11-02 22:17:31 -0400

    added DeleteAccTest

---
+++ Quinterac/test/DeleteAccTest.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class DeleteAccTest {
+    @Test
+    public void testAppR4T1() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "machine", "deleteacct"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Please use agent mode."),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T2() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Enter account number (7 digits):"),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T3() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "9999999"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Please enter a valid account number"),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T4() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "1234567"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Enter account name (3 to 30 alphanumeric characters):"),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T5() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "1234567", "qwerty", "deposit"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Account has been deleted. Please logout."),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T6() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "1234567", "qwerty", "withdraw"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Account has been deleted. Please logout."),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T7() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "1234567", "qwerty", "transfer"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("Account has been deleted. Please logout."),
+                Arrays.asList(""));
+    }
+
+    @Test
+    public void testAppR4T8() throws Exception {
+        AppTest.runAndTest(Arrays.asList("login", "agent", "deleteacct", "1234567", "qwerty", "logout"),
+                Arrays.asList("1234567", "1234568"),
+                Arrays.asList("You have logged out."),
+                Arrays.asList("DEL 1234567 000 0000000 qwerty", "EOS 0000000 000 0000000 ***"));
+    }
+}
