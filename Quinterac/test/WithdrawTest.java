2a0fecb6ca0f538d2a38f6e21f0035da5317c27e
polly-xuzy <16zx8@queensu.ca>
2019-11-04 01:15:40 -0500

    Update WithdrawTest.java

--- Quinterac/test/WithdrawTest.java
+++ Quinterac/test/WithdrawTest.java
@@ -25,14 +25,14 @@ public class WithdrawTest {
 	@Test
 	public void testAppR6T4() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "withdraw", "1234567", "100000", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("You have logged out."),
+							Arrays.asList("You have exceeded the daily limit.","You have logged out."),
 							Arrays.asList("WDR 1234567 100000 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
 	@Test
 	public void testAppR6T5() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "withdraw", "1234567", "99999", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("You have logged out."),
+							Arrays.asList("You have exceeded the daily limit.","You have logged out."),
 							Arrays.asList("WDR 1234567 99999 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
@@ -46,14 +46,14 @@ public class WithdrawTest {
 	@Test
 	public void testAppR6T7() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "agent", "withdraw", "1234567", "99999999", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("You have logged out."),
+							Arrays.asList("Withdraw successfully!","You have logged out."),
 							Arrays.asList("WDR 1234567 99999999 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
 	@Test
 	public void testAppR6T8() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "agent", "withdraw", "1234567", "99999998", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("You have logged out."),
+							Arrays.asList("Withdraw successfully!","You have logged out."),
 							Arrays.asList("WDR 1234567 99999998 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
@@ -61,7 +61,7 @@ public class WithdrawTest {
 	public void testAppR6T9() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000","withdraw", "1234567", "100000","withdraw", "1234567", "1","logout"), 
 							Arrays.asList("1234567"),
-							Arrays.asList("You have logged out."),
+							Arrays.asList("You have exceeded the daily limit.","You have logged out."),
 							Arrays.asList("WDR 1234567 10000000 0000000 ***", "WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","EOS 0000000 000 0000000 ***"));
 	}
 	
@@ -69,7 +69,7 @@ public class WithdrawTest {
 	public void testAppR6T10() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000", "withdraw", "1234567", "100000","withdraw", "1234567", "100000","logout"),
 							Arrays.asList("1234567"),			   
-							Arrays.asList("You have logged out."),
+							Arrays.asList("Withdraw successfully!","You have logged out."),
 							Arrays.asList("WDR 1234567 10000000 0000000 ***", "WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","WDR 1234567 10000000 0000000 ***","EOS 0000000 000 0000000 ***"));
 	}
 	
