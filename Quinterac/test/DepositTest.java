ed37224236a90c533a3fdb2ea453b43ce4b4c93f
polly-xuzy <16zx8@queensu.ca>
2019-11-04 01:02:28 -0500

    Update DepositTest.java

--- Quinterac/test/DepositTest.java
+++ Quinterac/test/DepositTest.java
@@ -25,14 +25,14 @@ public class DepositTest {
 	@Test
 	public void testAppR5T4() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "deposit", "1234567", "200000", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("Deposit successfully!", "You have logged out."), 
+							Arrays.asList("Deposit successfully:", "You have logged out."), 
 							Arrays.asList("DEP 1234567 200000 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
 	@Test
 	public void testAppR5T5() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "deposit", "1234567", "199999", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("Deposit successfully!", "You have logged out."), 
+							Arrays.asList("Deposit successfully:", "You have logged out."), 
 							Arrays.asList("DEP 1234567 199999 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
@@ -46,14 +46,14 @@ public class DepositTest {
 	@Test
 	public void testAppR5T7() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "agent", "deposit", "1234567", "99999999", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("Deposit successfully!", "You have logged out."),
+							Arrays.asList("Deposit successfully:", "You have logged out."),
 							Arrays.asList("DEP 1234567 99999999 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
 	@Test
 	public void testAppR5T8() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "agent", "deposit", "1234567", "99999998", "logout"), Arrays.asList("1234567"),
-							Arrays.asList("Deposit successfully!", "You have logged out."),
+							Arrays.asList("Deposit successfully:", "You have logged out."),
 							Arrays.asList("DEP 1234567 99999998 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
@@ -69,7 +69,7 @@ public class DepositTest {
 	public void testAppR5T10() throws Exception {
 		AppTest.runAndTest(Arrays.asList("login", "machine", "deposit", "1234567", "200000", "deposit", "1234567", "200000", "deposit", "1234567", "100000", "logout"),
 							Arrays.asList("1234567"),
-							Arrays.asList("Deposit successfullu!", "You have logged out."),
+							Arrays.asList("Deposit successfully:", "You have logged out."),
 							Arrays.asList("DEP 1234567 20000000 0000000 ***","DEP 1234567 20000000 0000000 ***","DEP 1234567 10000000 0000000 ***", "EOS 0000000 000 0000000 ***"));
 	}
 	
