f83a173d7df869dc49f6e1c7936c41bc7956af2f
polly-xuzy <16zx8@queensu.ca>
2019-11-03 21:59:34 -0500

    modularised batch files

---
+++ Quinterac/test-withdraw.bat
@@ -0,0 +1,22 @@
+::run WithdrawTest
+:WithdrawTest
+ECHO WithdrawTest#testAppR6T1
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T1 -q test
+ECHO WithdrawTest#testAppR6T2
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T2 -q test
+ECHO WithdrawTest#testAppR6T3
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T3 -q test
+ECHO WithdrawTest#testAppR6T4
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T4 -q test
+ECHO WithdrawTest#testAppR6T5
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T5 -q test
+ECHO WithdrawTest#testAppR6T6
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T6 -q test
+ECHO WithdrawTest#testAppR6T7
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T7 -q test
+ECHO WithdrawTest#testAppR6T8
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T8 -q test
+ECHO WithdrawTest#testAppR6T9
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T9 -q test
+ECHO WithdrawTest#testAppR6T10
+CALL mvn clean -Dtest=WithdrawTest#testAppR6T10 -q test
