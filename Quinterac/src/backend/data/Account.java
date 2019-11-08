7837e634554ee92aac86b7ebbe4642780acec8af
LeBarsQuentin <57099066+LeBarsQuentin@users.noreply.github.com>
2019-11-08 16:17:26 -0500

    Update Account.java

--- Quinterac/src/backend/data/Account.java
+++ Quinterac/src/backend/data/Account.java
@@ -1,4 +1,35 @@
 package backend.data;
 
 public class Account {
+  private String accNum;// number of the account
+  private int amount;//amount of money in the account
+  private String accName;// name of the accoount
+  
+  //creation of a new account
+  public Acc (String accNum, String accName, String amount) {
+		this.accNum = accNum;
+		this.accName = accName;
+    this.amount = Integer.parseInt(amount);
+	}
+  
+  //return the amount of money in the account
+  public int getAmount () {
+    return this.amount;
+  }
+  
+  //return the name of the account
+  public String getAccName () {
+    return this.accName;
+  }
+  
+  //return the number of the account
+  public String getAccNum () {
+    return this.accNum;
+  }
+  
+  //change the money in the account for the value given as a 
+  public void setAmount(int amount){
+    this.amount = amount;
+  }
+  
 }
