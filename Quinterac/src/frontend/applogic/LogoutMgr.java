b1637de97dfd1360770fd258ba35e158aaf46f27
YingTing04 <yingting2404@gmail.com>
2019-12-03 14:58:23 -0500

    added return to logout

--- Quinterac/src/frontend/applogic/LogoutMgr.java
+++ Quinterac/src/frontend/applogic/LogoutMgr.java
@@ -1,20 +1,23 @@
 //the objective of this class is to manage logout operations and exceptions
 
 package frontend.applogic;
+
 import frontend.exceptions.NotLoggedInException;
 
 public class LogoutMgr {
-	//method to perform logout operation
-    public static void logout(String transactionSummaryFile) {
-        try {
-            if (LoginMgr.isLoggedIn()) {
-                LoginMgr.resetLogin();
-                TransactionFileMgr.writeToTransactionFile(transactionSummaryFile);
-                TransactionFileMgr.resetTransactionList();
-                AccMgr.resetDailyAccMap();
-                System.out.println("You have logged out.");
-            }
-            else throw new NotLoggedInException();
-        } catch (NotLoggedInException e) { System.out.println(e.getMessage()); }
-    }
+	// method to perform logout operation
+	public static void logout(String transactionSummaryFile) {
+		try {
+			if (LoginMgr.isLoggedIn()) {
+				LoginMgr.resetLogin();
+				TransactionFileMgr.writeToTransactionFile(transactionSummaryFile);
+				TransactionFileMgr.resetTransactionList();
+				AccMgr.resetDailyAccMap();
+				System.out.println("You have logged out.");
+			} else
+				throw new NotLoggedInException();
+		} catch (NotLoggedInException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
--- Quinterac/src/main/Frontend.java
+++ Quinterac/src/main/Frontend.java
@@ -2,25 +2,49 @@
 
 package main;
 
-import frontend.applogic.*;
+import frontend.applogic.CreateAccMgr;
+import frontend.applogic.DeleteAccMgr;
+import frontend.applogic.DepositMgr;
+import frontend.applogic.LoginMgr;
+import frontend.applogic.LogoutMgr;
+import frontend.applogic.TransferMgr;
+import frontend.applogic.WithdrawMgr;
 
 public class Frontend {
-	//method to start frontend program
-    public static void mainFrontend(String accList, String transactionSummaryFile) {
-        System.out.println("Welcome to Quinterac (Frontend)! To start, please login.");
-        while (Quinterac.s.hasNextLine()){
-            //Scanner s = new Scanner(System.in);
-            String transactionCode = Quinterac.s.nextLine().trim();
-            switch (transactionCode) {
-                case "login": LoginMgr.login(accList); break;
-                case "logout": LogoutMgr.logout(transactionSummaryFile); break;
-                case "deposit": DepositMgr.deposit(); break;
-                case "withdraw": WithdrawMgr.withdraw(); break;
-                case "transfer": TransferMgr.transfer(); break;
-                case "createacct": CreateAccMgr.createacc(); break;
-                case "deleteacct": DeleteAccMgr.deleteacc(); break;
-                default: System.out.println("Please enter a valid transaction code."); break;
-            }
-        }
-    }
+	// method to start frontend program
+	static int count = 0;
+
+	public static void mainFrontend(String accList, String transactionSummaryFile) {
+		System.out.println("Welcome to Quinterac (Frontend)! To start, please login.");
+		while (Quinterac.s.hasNextLine()) {
+			// Scanner s = new Scanner(System.in);
+			String transactionCode = Quinterac.s.nextLine().trim();
+			switch (transactionCode) {
+			case "login":
+				LoginMgr.login(accList);
+				break;
+			case "logout":
+				LogoutMgr.logout(transactionSummaryFile);
+				return;
+			case "deposit":
+				DepositMgr.deposit();
+				break;
+			case "withdraw":
+				WithdrawMgr.withdraw();
+				break;
+			case "transfer":
+				TransferMgr.transfer();
+				break;
+			case "createacct":
+				CreateAccMgr.createacc();
+				break;
+			case "deleteacct":
+				DeleteAccMgr.deleteacc();
+				break;
+			default:
+				System.out.println("Please enter a valid transaction code.");
+				break;
+			}
+		}
+	}
 }
