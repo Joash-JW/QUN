f3d7c14a37e98bb69c07492d792b2d67c8707a98
YingTing04 <yingting2404@gmail.com>
2019-12-03 16:55:51 -0500

    updated py script

--- Quinterac/integration-testing.bat
+++
@@ -1,4 +0,0 @@
-::run Quinterac
-CALL mvn compile
-CALL cd target/classes
-CALL java main/Quinterac frontend ../../ValidAccList.txt 123
--- Quinterac/integration.py
+++ Quinterac/integration.py
@@ -6,12 +6,62 @@ import pyautogui
 print("Integration Testing...")
 os.system("mvn compile")
 os.chdir("./target/classes")
+validAcc = "../../ValidAccList.txt "
+transSumDir = "../../TransactionFiles/"
+master = "../../Master.txt "
 
-def runJava():
-    os.system("java main/Quinterac frontend ../../ValidAccList.txt 123")
+def runJava(session, transSum):
+    os.system("java main/Quinterac "+session+" ../../ValidAccList.txt " + transSum)
 
-t = threading.Thread(target=runJava, args=())
+t = threading.Thread(target=runJava, args=("frontend", "../../TransactionFiles/TransSum1.txt",))
 t.start()
-time.sleep(3) # important for race conditions
+time.sleep(1) # important for race conditions
 pyautogui.write("login")
 pyautogui.press("enter")
+pyautogui.write("agent")
+pyautogui.press("enter")
+pyautogui.write("createacct")
+pyautogui.press("enter")
+pyautogui.write("1234567")
+pyautogui.press("enter")
+pyautogui.write("qwerty")
+pyautogui.press("enter")
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+t = threading.Thread(target=runJava, args=("frontend","../../TransactionFiles/TransSum2.txt",))
+t.start()
+time.sleep(1) # important for race conditions
+pyautogui.write("login")
+pyautogui.press("enter")
+pyautogui.write("agent")
+pyautogui.press("enter")
+pyautogui.write("createacct")
+pyautogui.press("enter")
+pyautogui.write("1234568")
+pyautogui.press("enter")
+pyautogui.write("abcde")
+pyautogui.press("enter")
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+t = threading.Thread(target=runJava, args=("frontend","../../TransactionFiles/TransSum3.txt",))
+t.start()
+time.sleep(1) # important for race conditions
+pyautogui.write("login")
+pyautogui.press("enter")
+pyautogui.write("agent")
+pyautogui.press("enter")
+pyautogui.write("createacct")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+pyautogui.write("uwxyz")
+pyautogui.press("enter")
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+
+t = threading.Thread(target=runJava, args=("backend","../../MergeTransSum.txt",))
+t.start()
+time.sleep(5) # important for race conditions
