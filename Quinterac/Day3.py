d0c372503c01e68a8dbf8ad85ae3af3fcaaef1ad
LeBarsQuentin <57099066+LeBarsQuentin@users.noreply.github.com>
2019-12-06 18:52:48 -0500

    Create Day3.py

---
+++ Quinterac/Day3.py
@@ -0,0 +1,122 @@
+import os
+import time
+import threading
+import pyautogui
+
+print("Integration Testing...")
+os.system("mvn compile")
+os.chdir("./target/classes")
+
+validAcc = " ../../ValidAccList.txt "
+transSumDir = "../../TransactionFiles/"
+master = " ../../Master.txt "
+
+def runJava(session, arg1, arg2):
+    os.system("java main/Quinterac "+session+ arg1 + arg2)
+
+t = threading.Thread(target=runJava, args=("frontend", validAcc, transSumDir+"TransSum1.txt",))
+t.start()
+time.sleep(1) # important for race conditions
+pyautogui.write("login")
+pyautogui.press("enter")
+pyautogui.write("machine")
+pyautogui.press("enter")
+
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234567")
+pyautogui.press("enter")
+pyautogui.write("1500")
+pyautogui.press("enter")
+
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234567")
+pyautogui.press("enter")
+pyautogui.write("1234568")
+pyautogui.press("enter")
+pyautogui.write("999999999")
+pyautogui.press("enter")
+
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234567")
+pyautogui.press("enter")
+pyautogui.write("1234568")
+pyautogui.press("enter")
+pyautogui.write("0")
+pyautogui.press("enter")
+
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+t = threading.Thread(target=runJava, args=("frontend", validAcc, transSumDir+"TransSum2.txt",))
+t.start()
+time.sleep(1) # important for race conditions
+pyautogui.write("login")
+pyautogui.press("enter")
+pyautogui.write("machine")
+pyautogui.press("enter")
+
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234568")
+pyautogui.press("enter")
+pyautogui.write("2500")
+pyautogui.press("enter")
+
+
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+t = threading.Thread(target=runJava, args=("frontend", validAcc, transSumDir+"TransSum3.txt",))
+t.start()
+time.sleep(1) # important for race conditions
+pyautogui.write("login")
+pyautogui.press("enter")
+pyautogui.write("agent")
+pyautogui.press("enter")
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+pyautogui.write("8000")
+pyautogui.press("enter")
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234568")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+pyautogui.write("11000")
+pyautogui.press("enter")
+
+pyautogui.write("transfer")
+pyautogui.press("enter")
+pyautogui.write("1234569")
+pyautogui.press("enter")
+pyautogui.write("1234567")
+pyautogui.press("enter")
+pyautogui.write("0")
+pyautogui.press("enter")
+
+pyautogui.write("logout")
+pyautogui.press("enter")
+
+
+t = threading.Thread(target=runJava, args=("backend", master, "../../MergeTransSum.txt",))
+t.start()
+time.sleep(5) # important for race conditions
